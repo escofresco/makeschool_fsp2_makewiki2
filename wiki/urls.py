d3683de19699dfcea0646f6c5be05a553f41e9cf
Jonasz Karwacki <jonaszakr@gmail.com>
2019-11-18 20:27:26 -0800

    fix url issue

--- wiki/urls.py
+++ wiki/urls.py
@@ -6,5 +6,5 @@ from wiki.views import PageListView, PageCreateView,PageDetailView
 urlpatterns = [
     path('', PageListView.as_view(), name='wiki-list-page'),
     path('<str:slug>/', PageDetailView.as_view(), name='wiki-details-page'),
-    path('create/', PageCreateView.as_view(), name='wiki-create-page')
+    path('page/create/', PageCreateView.as_view(), name='wiki-create-page')
 ]
