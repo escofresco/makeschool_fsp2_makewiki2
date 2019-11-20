3c8d0d976fcab1c4e8297417b46f6f14ac93f224
Jonasz Karwacki <jonaszakr@gmail.com>
2019-11-20 14:28:28 -0800

    change order of urls

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from wiki.views import PageListView, PageCreateView,PageDetailView
-
+from wiki.views import PageListView, PageCreateView, PageDetailView
 
 urlpatterns = [
     path('', PageListView.as_view(), name='wiki-list-page'),
+    path('create/', PageCreateView.as_view(), name='wiki-create-page'),
     path('<str:slug>/', PageDetailView.as_view(), name='wiki-details-page'),
-    path('page/create/', PageCreateView.as_view(), name='wiki-create-page')
 ]
