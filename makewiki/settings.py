7ce88fc10f6e4bed22a368fc341a4f0a2f524dcf
Jonasz Karwacki <jonaszakr@gmail.com>
2019-12-04 14:01:59 -0800

    fix staticfiles typo

--- makewiki/settings.py
+++ makewiki/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '1yct-t!2bnkgc7j59z+9cdd2k)@y+ftqor$!aya()3if^cnlo-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "make-wiki.herokuapp.com"]
+ALLOWED_HOSTS = ["0.0.0.0", "make-wiki.herokuapp.com"]
 
 # Application definition
 
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, STATICFILES_DIRS),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 # wiki app settings
